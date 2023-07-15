7caeaae67a29a152105948009234d5183762a51b
Karo <karolp2003@gmail.com>
2023-07-15 14:31:23 +0200

    Liked/no list

---
+++ components/LikedButton.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+
+import { toast } from "react-hot-toast";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+import { useUser } from "@/hooks/useUser";
+import useAuthModal from "@/hooks/useAuthModal";
+
+interface LikeButtonProps {
+  songId: string;
+}
+
+const LikedButton = ({ songId }: LikeButtonProps) => {
+  const router = useRouter();
+  const { supabaseClient } = useSessionContext();
+  const authModal = useAuthModal();
+  const { user } = useUser();
+
+  const [isLiked, setIsLiked] = useState(false);
+
+  useEffect(() => {
+    if (!user?.id) {
+      return;
+    }
+
+    (async () => {
+      const { data, error } = await supabaseClient
+        .from("liked_songs")
+        .select("*")
+        .eq("user_id", user.id)
+        .eq("song_id", songId)
+        .single();
+
+      if (!error && data) {
+        setIsLiked(true);
+      }
+    })();
+  }, [songId, supabaseClient, user?.id]);
+
+  const handleLike = async () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    if (isLiked) {
+      const { error } = await supabaseClient
+        .from("liked_songs")
+        .delete()
+        .eq("user_id", user.id);
+
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(false);
+      }
+    } else {
+      const { error } = await supabaseClient.from("liked_songs").insert({
+        song_id: songId,
+        user_id: user.id,
+      });
+      if (error) {
+        toast.error(error.message);
+      } else {
+        setIsLiked(true);
+      }
+    }
+  };
+
+  const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
+
+  return (
+    <button onClick={handleLike} className="hover:opacity-75 transition">
+      <Icon color={isLiked ? "#22c55e" : "white"} size={25} />
+    </button>
+  );
+};
+
+export default LikedButton;
